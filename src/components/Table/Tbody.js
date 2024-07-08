e5c26f4ec0dcdf19a0a726ec89986d7546ee209f
MishC <michaela.chovancova@gmail.com>
2024-07-08 12:27:50 +0200

    Update Tbody.js

--- src/components/Table/Tbody.js
+++ src/components/Table/Tbody.js
@@ -1,18 +1,24 @@
 import "./Table.css";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getUserById } from "../../backend/api";
+import { getUser } from "../../actions/shared";
 
-const Tbody = ({ questions }) => {
+const Tbody = ({ questions,answered}) => {
 
   return (
     <tbody>
       {questions.map((item, id) => {
         const date = new Date(item.timestamp);
+        
         return (
           <tr key={id}>
             <td>{item.author}</td>
-            <td>{date.toLocaleString('no')}</td>
+            <td>{date.toLocaleDateString('no')}</td>
             <td>
-              <button>See More</button>
+            <Link to={`/questions/${item.id}`}>
+                <button>{answered?`See details`:`Vote`}</button>
+              </Link>
             </td>
           </tr>
         );
@@ -20,5 +26,6 @@ const Tbody = ({ questions }) => {
     </tbody>
   );
 };
+//const mapStateToProps=({user})=>({user});
 
-export default Tbody;
+export default connect()(Tbody);
