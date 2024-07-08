10c8d324935e32e3f3879f5868b09b34840409f1
MishC <michaela.chovancova@gmail.com>
2024-07-08 12:27:54 +0200

    Update Unanswered.js

--- src/components/Unanswered/Unanswered.js
+++ src/components/Unanswered/Unanswered.js
@@ -1,18 +1,16 @@
 import "./Uanswered.css";
-import { useEffect } from "react";
 import { connect } from "react-redux";
 import Table from "../Table/Table";
-import { getUnansweredQuestions } from "../../actions/shared";
 
 const Unanswered = ({ user, questions, dispatch }) => {
   
 
   return (
     <div className="Unanswered">
-     {/* <h2>Unanswered Polls</h2>*/}
+    
       {questions.unanswered ? (
        
-          <Table questions={questions.unanswered} />
+          <Table questions={questions.unanswered} answered={false}/>
          
       ) : (
         ""
