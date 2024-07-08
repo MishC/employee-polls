0bc7dabc9e99d0abb0426c0114d7ed170360c941
MishC <michaela.chovancova@gmail.com>
2024-07-08 12:26:51 +0200

    Updated Answered component

--- src/components/Answered/Answered.js
+++ src/components/Answered/Answered.js
@@ -1,20 +1,12 @@
 
 import Table from "../Table/Table";
 import { connect } from "react-redux";
-import { useEffect } from "react";
-import { getAnsweredQuestions } from "../../actions/shared";
 
 const Answered = ({user,questions,dispatch}) => {
-    useEffect(()=>{
-        if (!user){console.log("User is not defined!")}
-        
-          dispatch(getAnsweredQuestions(user));
-        },[user,dispatch]);
    
     return (
         <div className="Answered">
-            {/*<h2>  Answered Polls  </h2>*/}
-       {questions.answered&&     <Table questions={Object.values(questions.answered)}/>}
+       {questions.answered&&     <Table questions={Object.values(questions.answered)} answered={true}/>}
 
              </div>
     )
