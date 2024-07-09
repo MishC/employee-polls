cba6f15979883a1d6ac682530d70cc52d3be6bec
MishC <michaela.chovancova@gmail.com>
2024-07-09 09:10:02 +0200

    Update Vote.js

--- src/components/QuestionDetail.js/Vote.js
+++ src/components/QuestionDetail.js/Vote.js
@@ -1,28 +1,40 @@
 import "./Vote.css";
 import React, { useState } from 'react';
+import { connect } from "react-redux";
+import { saveQuestionAnswer } from "../../actions/saveVote";
 
-const Vote = ({question}) => {
+const Vote = ({question, authedUser, saveQuestionAnswer}) => {
 
     const [selectedOption, setSelectedOption] = useState('');
 
-  const handleOptionChange = (e) => {
+  const handleChange = (e) => {
     setSelectedOption(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`Selected option: ${selectedOption}`);
+    if (selectedOption) {
+      saveQuestionAnswer({ authedUser, qid: question.id, answer: selectedOption });
+    }
   };
+
+  if (!question) {
+    return <h2 className="error"> Oops, question not found!</h2>;
+  }
+
+  
     return (<div className="Vote">
    
     <form onSubmit={handleSubmit}>
       <div>
       <label class="container">A
-  <input type="radio" checked="checked" name="radio"/>
+  <input type="radio" checked={selectedOption === 'optionOne'}
+          onChange={handleChange} name="optionOne"/>
   <span class="checkmark"></span>
 </label>
 <label class="container">B
-  <input type="radio" name="radio"/>
+  <input type="radio" checked={selectedOption === 'optionTwo'}
+          onChange={handleChange} name="optionTwo" />
   <span class="checkmark"></span>
 </label>
       </div>
@@ -34,4 +46,19 @@ const Vote = ({question}) => {
     </div>)
 }
 
-export default Vote;
+const mapStateToProps = (state, question_id) => {
+    const question = state.questions[question_id];
+    const authedUser = state.authedUser;
+  
+    return {
+      question,
+      authedUser
+    };
+  };
+  
+  const mapDispatchToProps = {
+    saveQuestionAnswer
+  };
+  
+export default connect(mapStateToProps, mapDispatchToProps)(Vote)
+
