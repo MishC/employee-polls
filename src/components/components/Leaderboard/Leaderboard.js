c3fc6064f83b19d510fa1876dee7b9f66d30e609
MishC <michaela.chovancova@gmail.com>
2024-08-09 11:25:50 +0200

    delete

--- src/components/components/Leaderboard/Leaderboard.js
+++
@@ -1,6 +0,0 @@
-const Leaderboard = () => {
-    return (<div className="Leaderboard">
-    </div>);
-}
-
-export default Leaderboard;
--- src/middlewares/middlewares/middleware.js
+++
@@ -1,7 +0,0 @@
-import {thunk} from "redux-thunk";
-import logger from "./logger";
-import authCheck from "./authCheck";
-
-const middleware = [thunk, logger];
-
-export default middleware;
