f30462439a966f799e322037845d8c83a41241c4
MishC <michaela.chovancova@gmail.com>
2024-08-09 11:29:14 +0200

    deleting comments

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ import { createRoot } from "react-dom/client";
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { Provider } from "react-redux";
 import reducer from "./reducers";
-import middleware from './middlewares/middleware'; // Import your custom middleware correctly
+import middleware from './middlewares/middleware'; 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// Configure Redux store with reducers and middleware
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware),
 });
 
-// Render the app inside a root created with `createRoot`
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <Router>
