c3895264e677deda24acaae2e78b55c1a05f9631
MishC <michaela.chovancova@gmail.com>
2024-08-10 16:04:55 +0200

    navigate("/")

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -5,6 +5,9 @@ import Rank from "./Rank";
 import { useDispatch, connect } from "react-redux";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrophy} from '@fortawesome/free-solid-svg-icons'; 
+import { useNavigate } from 'react-router-dom';
+
+
 const Leaderboard = ({users}) => {
     const dispatch = useDispatch();
     useEffect(() => {
@@ -12,7 +15,12 @@ const Leaderboard = ({users}) => {
      const users=dispatch(getAllUsers());
      console.log(users);
      }, []);
+     const navigate = useNavigate();
 
+     const handleNavigate = () => {
+       navigate('/');
+     };
+   
     return (<div className="Leaderboard">
         <div className="leaderboard-title">
         <h1>Leaderboard <FontAwesomeIcon  icon={faTrophy} size="2x" /></h1></div>
@@ -35,7 +43,7 @@ const Leaderboard = ({users}) => {
     </table>
 
     
-          <button className="back-button">Home</button>
+          <button className="back-button" onClick={handleNavigate}>Home</button>
 
     </div>);
 }
