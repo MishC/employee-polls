6a0ea93a49f71e735bf1d656e58fd9c1aa9eabdb
MishC <michaela.chovancova@gmail.com>
2024-08-11 01:56:03 +0200

    sorting user in reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -10,8 +10,8 @@ case RECEIVE_ALL_USERS:
         return {
           ...state,
      ...Object.values(action.users).map(u=>{return [u.name,u.questions.length,
-        Object.keys(u.answers).length]})
-    
+        Object.keys(u.answers).length]}).sort((a, b) => (b[1]+b[2]) - (a[1]+a[2]))
+
        }
 default:
       return state;
